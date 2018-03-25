2b1cec5b167f229614f9aa3ac2ddd10b74d8ef3f
Yan Bai <Yan.Bai@veritas.com>
2018-03-24 23:08:36 -0700

    Add 5. Longest Palindromic Substring

---
+++ LongestPalindromicSubstring.java
@@ -0,0 +1,62 @@
+// 5. Longest Palindromic Substring
+
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("a"));
+    }
+
+    public static String longestPalindrome(String s) {
+        if(s == null || s.length() == 0) return s;
+
+        int len = s.length(), maxLeft = 0, maxRight = 0;
+        boolean[][] dp = new boolean[len][len];
+
+        for(int span=1; span<=len; span++) {
+            for(int i=0; i<len-span+1; i++) {
+                if( s.charAt(i) == s.charAt(i+span-1) && (span<4 || dp[i+1][i+span-2]) ) {
+                    dp[i][i+span-1] = true;
+                    if(span > maxRight - maxLeft){
+                        maxLeft = i;
+                        maxRight = i + span;
+                    }
+                }
+            }
+        }
+
+        return s.substring(maxLeft, maxRight);
+    }
+
+    /*
+    public static String longestPalindrome(String s) {
+        if(s == null || s.length() == 0) return "";
+
+        int[] longest = new int[]{0, 0};
+
+        for(int i=0; i<s.length(); i++) {
+            int[] odd = helper(s, i, i);
+            int[] even = helper(s, i, i+1);
+            int[] tmp = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+            longest = tmp[1] - tmp[0] > longest[1] - longest[0] ? tmp : longest; 
+        }
+
+        return s.substring(longest[0]+1, longest[1]);
+    }
+
+    private static int[] helper(String s, int left, int right){
+        int len = s.length();
+        int[] ret = new int[]{0, 0};
+
+        while(left >=0 && right < len && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        ret[0] = left;
+        ret[1] = right;
+
+        return ret; 
+    }
+    */ 
+}
+
