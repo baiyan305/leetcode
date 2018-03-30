1115f36060a7a1a4d94a6cf9bf1bc4693b9114e1
Yan Bai <Yan.Bai@veritas.com>
2018-03-30 14:22:16 -0700

    Update 72. Edit Distance

--- EditDistance.java
+++ EditDistance.java
@@ -3,10 +3,38 @@
 class EditDistance {
 
     public static void main(String[] args){
-        System.out.println(minDistance("aefasd","abasxx"));
+        System.out.println(minDistance("aefasd","abas"));
+        System.out.println(minDistance1("aefasd","abas"));
     }
 
     public static int minDistance(String word1, String word2) {
+        int len1 = word1.length(), len2 = word2.length();
+        int[] dp = new int[len1+1];
+
+        for(int i=0; i<len1+1; i++) {
+            dp[i] = i;
+        }
+
+        for(int i=1; i<len2+1; i++) {
+            int tmp = dp[0];
+            dp[0] = i;
+            for(int j=1; j<len1+1; j++) {
+                // replace operation
+                int replaceOperation = word1.charAt(j-1) == word2.charAt(i-1) ? tmp : tmp + 1;
+                // remove operation
+                int removeOperation = dp[j-1] + 1;
+                // insert operation
+                int insertOperation = dp[j] + 1;
+
+                tmp = dp[j];
+                dp[j] = Math.min(replaceOperation, Math.min(removeOperation, insertOperation));
+            }
+        }
+
+        return dp[len1];
+    }
+    
+    public static int minDistance1(String word1, String word2) {
         int len1 = word1.length(), len2 = word2.length();
         int[][] dp = new int[len1+1][len2+1];
 
@@ -30,5 +58,5 @@ class EditDistance {
 
         return dp[len1][len2];
     }
-
+    
 }
