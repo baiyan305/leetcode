a61bb015098c9e887cd99c407377963a5d0690a7
Yan Bai <baiyan718@gmail.com>
2018-04-02 21:42:28 -0700

    Add 122. Best Time to Buy and Sell Stock II
    Add 122. Best Time to Buy and Sell Stock II

---
+++ BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,20 @@
+// 122. Best Time to Buy and Sell Stock II
+
+public class BestTimeToBuyAndSellStockII {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) return 0;
+		
+		int profit = 0;
+		for(int i=1; i<prices.length; i++) {
+			if(prices[i] > prices[i-1]) total += prices[i] - prices[i-1];
+		}
+		
+		return profit;
+    }
+	
+}
