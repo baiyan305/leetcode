c25ccf98e455782df7070ad449c3ebfc834bb04d
Yan Bai <Yan.Bai@veritas.com>
2018-04-18 21:53:47 -0700

    Add 321. Create Maximum Number

---
+++ CreateMaximumNumber.java
@@ -0,0 +1,28 @@
+// 321. Create Maximum Number
+
+public class CreateMaximumNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int len1 = nums1.length, len2 = nums.length;    
+    
+        for(int i=1; i<=len1 && k-i<=len2; i++) {
+                
+        }
+    }
+
+    private int[] getSubArray(int[] arr, int k) {
+        int[] max = new int[k];
+        for(int i=arr.length-1; i--; i>=0) {
+            
+        }  
+    }
+
+    private int[] merge(int[] arr1, int[] arr2) {
+    
+    }
+
+}
