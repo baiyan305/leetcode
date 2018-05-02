8c025707a301fe0937440f055c09fc26b5c15afa
Yan Bai <Yan.Bai@veritas.com>
2018-05-02 14:47:59 -0700

    Add 357. Count Numbers with Unique Digits

---
+++ CountNumbersWithUniqueDigits.java
@@ -0,0 +1,37 @@
+// 357. Count Numbers with Unique Digits
+
+public class CountNumbersWithUniqueDigits {
+
+    public static void main(String[] args) {
+        System.out.println(countNumbersWithUniqueDigits(2)); 
+    }
+
+    static boolean[] used = new boolean[10];
+    static int total = 1;
+    public static int countNumbersWithUniqueDigits(int n) {
+        if(n==0) return 1;
+
+        int max = (int)Math.pow(10, n);
+        for(int i=1; i<10; i++) {
+            used[i] = true;
+            backtracking(1, max);
+            used[i] = false;
+        }
+        
+        return total;
+    }
+
+    private static void backtracking(int num, int max) {
+        if(num < max) total += 1;
+        else return;
+
+        for(int i=0; i<10; i++) {
+            if(!used[i]) {
+                used[i] = true;
+                backtracking(num*10+i, max);
+                used[i] = false;
+            }
+        }
+    }
+
+}
