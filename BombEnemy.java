d1a03ba9e1649a3747731905c3702cbd840f9160
Yan Bai <Yan.Bai@veritas.com>
2018-05-02 20:52:17 -0700

    Add 361. Bomb Enemy

---
+++ BombEnemy.java
@@ -0,0 +1,76 @@
+// 361. Bomb Enemy
+
+public class BombEnemy {
+
+    public static void main(String[] args) {
+        char[][] grid = {
+            {'0','E','0','0'},
+            {'E','0','W','E'},
+            {'0','E','0','0'}
+        };
+
+        System.out.println(maxKilledEnemies(grid));
+    }
+
+    public static int maxKilledEnemies(char[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
+
+        int height = grid.length, width = grid[0].length;
+        int[][] left = new int[height][width];
+        int[][] right = new int[height][width];
+        int[][] above = new int[height][width];
+        int[][] below = new int[height][width];
+
+        for(int h=0; h<height; h++) {
+            int sum = 0;
+            for(int w=0; w<width; w++) {
+                char c = grid[h][w];
+                if(c == 'W') sum = 0;
+                else if(c == 'E') sum++;
+                left[h][w] = sum;        
+            }
+        }
+
+        for(int h=0; h<height; h++) {
+            int sum = 0;
+            for(int w=width-1; w>=0; w--) {
+                char c = grid[h][w];
+                if(c == 'W') sum = 0;
+                else if(c == 'E') sum++;
+                right[h][w] = sum;        
+            }
+        }
+
+        for(int w=0; w<width; w++) {
+            int sum = 0;
+            for(int h=0; h<height; h++) {
+                char c = grid[h][w];
+                if(c == 'W') sum = 0;
+                else if(c == 'E') sum++;
+                above[h][w] = sum;        
+            }
+        }
+
+        for(int w=0; w<width; w++) {
+            int sum = 0;
+            for(int h=height-1; h>=0; h--) {
+                char c = grid[h][w];
+                if(c == 'W') sum = 0;
+                else if(c == 'E') sum++;
+                below[h][w] = sum;        
+            }
+        }
+
+        int max = 0;
+        for(int h=0; h<height; h++) {
+            for(int w=0; w<width; w++) {
+                if(grid[h][w] == '0'){
+                    max = Math.max(max, left[h][w]+right[h][w]+above[h][w]+below[h][w]);
+                }     
+            }
+        }
+
+        return max;
+    }
+
+}
