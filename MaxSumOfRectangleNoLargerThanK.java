5146694b74b7019e5ecdd99fc4092f1629f19edf
Yan Bai <baiyan718@gmail.com>
2018-05-07 21:55:43 -0700

    Update 363. Max Sum of Rectangle No Larger Than K

--- MaxSumOfRectangleNoLargerThanK.java
+++ MaxSumOfRectangleNoLargerThanK.java
@@ -1,32 +1,43 @@
 // 363. Max Sum of Rectangle No Larger Than K
 
+import java.util.TreeSet;
+
 public class MaxSumOfRectangleNoLargerThanK {
 
     public static void main(String[] args) {
 		int[][] matrix = {
-			{2,2,-1}
-			//{0,-2,3}
+			{2,2,-1},
+			{0,-2,3}
 		};
 		
-		System.out.println(maxSumSubmatrix(matrix, 0));
+		System.out.println(maxSumSubmatrix(matrix, 4));
     }
 
 	public static int maxSumSubmatrix(int[][] matrix, int k) {
 		int height = matrix.length, width = matrix[0].length, max = Integer.MIN_VALUE;
 		
-		for(int left=0; left<=width; left++) {
+		for(int left=0; left<width; left++) {
 			int[] sum = new int[height];
-			for(int right=left; right<=width; right++) {
+			for(int right=left; right<width; right++) {
 				for(int h=0; h<height; h++) {
-					sum[i] += matrix[h][right];
+					sum[h] += matrix[h][right];
+					//if(sum[i] == k) return k;
+				}
+				
+				TreeSet<Integer> set = new TreeSet<>();
+				set.add(0);
+				int tmp = 0;
+				for(int num : sum) {
+					tmp += num;
+					//if(tmp == k) return k;
+					Integer least = set.ceiling(tmp-k);
+					if(least != null) max = Math.max(max, tmp-least);
+					set.add(tmp);
 				}
 			}
 		}
-	}
-	
-	private static int binarySearch(int[] nums, int k) {
-		Arrays.sort(nums);
 		
+		return max;
 	}
 	
 	/*
