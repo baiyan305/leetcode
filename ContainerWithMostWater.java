9176f95a913109969f86b8a2ba54c56402a5c0f9
Yan Bai <baiyan718@gmail.com>
2018-05-27 18:04:25 -0700

    Add 11. Container With Most Water

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+// 11. Container With Most Water
+
+public class ContainerWithMostWater {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public int maxArea(int[] height) {
+        if(height == null || height.length < 2) return 0;
+		int max = 0, left = 0, right = height.length - 1;
+		
+		while(left<right) {
+			max = Math.max(max, Math.min(height[left], height[right]) * (right-left));
+			if(height[left] < height[right]) left++;
+			else if(height[left] > height[right) right--;
+			else {
+				left++;
+				right--;
+			}
+		}
+		
+		return max;
+    }
+	
+}
