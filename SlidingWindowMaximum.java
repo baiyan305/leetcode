a1eef750c875f3844f9cdcff2f2ccd75155c9473
Yan Bai <Yan.Bai@veritas.com>
2019-01-08 11:48:43 -0800

    Update 239. Sliding Window Maximum

--- SlidingWindowMaximum.java
+++ SlidingWindowMaximum.java
@@ -12,6 +12,31 @@ public class SlidingWindowMaximum {
         System.out.println("123");
     }
 
+    // 队列中的元素，是从大到小递减的。单调递减队列。
+    // 头部元素就是当前window最大值。
+    // 每次循环到一个新的元素时，从后往前，比较队列中的元素和新元素。
+    // 如果队列中的元素比新元素小，那么出队。
+    // 因为他们对于寻找后面window的最大值，已经没有意义了。
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        int[] res = new int[len-k+1];
+        Deque<Integer> queue = new LinkedList<>();
+
+        for(int i=0; i<len; i++) {
+            // 如果队列头部元素，比window范围小，那么需要出队
+            if(i>=k && queue.getFirst() <= i-k) queue.removeFirst();
+            // 从后往前，比较队列中的元素和nums[i], 如果队列中的元素比nums[i]小，那么出队。
+            // 因为他们对于得到后面window的maximum已经没有意义了。
+            while(!queue.isEmpty() && nums[queue.getLast()] <= nums[i]) queue.removeLast();
+            // 新元素加入队列
+            queue.add(i);
+            //更新结果
+            if(i>=k-1) res[i-k+1] = nums[queue.getFirst()];
+        }
+
+        return res;
+    }
+
     public int[] maxSlidingWindow(int[] nums, int k) {
         if(nums==null || nums.length==0) return new int[0];
 
